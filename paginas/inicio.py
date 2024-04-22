731cc99175300e48debd8f5e8296a5f79ba2ff3c
RubenMaestre <formacion@rubenmaestre.com>
2024-04-22 17:26:56 +0200

    Update inicio.py

--- paginas/inicio.py
+++ paginas/inicio.py
@@ -2,8 +2,19 @@
 import streamlit as st
 
 def display():
+    font_link = "https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;700&display=swap"
+    st.markdown(f"<link href='{font_link}' rel='stylesheet'>", unsafe_allow_html=True)
 
-    st.markdown("<h1 style='text-align: center;'>Coast to Coast - USA</h1>", unsafe_allow_html=True)
+    # Usando la fuente en un elemento HTML
+    st.markdown("""
+        <style>
+            h1 {
+                font-family: 'Roboto', sans-serif;
+                text-align: center;
+            }
+        </style>
+        <h1>Coast to Coast - USA</h1>
+    """, unsafe_allow_html=True)
     st.image('sources/coast_to_coast.jpg', use_column_width=True)
     st.markdown("<br><br>", unsafe_allow_html=True)
 
