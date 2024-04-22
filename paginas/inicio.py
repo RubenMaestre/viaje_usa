3d47ffc4ba21bb3b821dabf798926cfcac57932f
RubenMaestre <formacion@rubenmaestre.com>
2024-04-22 17:22:56 +0200

    actualiza

--- paginas/inicio.py
+++ paginas/inicio.py
@@ -2,13 +2,9 @@
 import streamlit as st
 
 def display():
-    # Imagen de cabecera (si decides volver a incluirla)
-    # st.image('sources/cabecera.jpg', use_column_width=True)
 
-    st.markdown("<br><br>", unsafe_allow_html=True)  # Espacio extra
-
-    # Título de la página
     st.markdown("<h1 style='text-align: center;'>Coast to Coast - USA</h1>", unsafe_allow_html=True)
+    st.image('sources/coast_to_coast.jpg', use_column_width=True)
     st.markdown("<br><br>", unsafe_allow_html=True)
 
     # Introducción descriptiva
@@ -19,3 +15,4 @@ def display():
             los datos, coordenadas y demás, quiero compartir con vosotros aquellos bonitos recuerdos.
         </div>
         """, unsafe_allow_html=True)
+    st.markdown("<br><br>", unsafe_allow_html=True)  # Espacio extra
