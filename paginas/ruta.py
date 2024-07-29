e210853f3649804c5030297f2d0e28cd405f0817
RubenMaestre <formacion@rubenmaestre.com>
2024-07-29 18:29:23 +0200

    Update ruta.py

--- paginas/ruta.py
+++ paginas/ruta.py
@@ -9,6 +9,25 @@ def display():
     st.markdown("<h1 style='text-align: center;'>Ruta</h1>", unsafe_allow_html=True)
     st.markdown("<br><br>", unsafe_allow_html=True)
 
+    # Introducción del viaje
+    st.markdown("""
+        <div style='text-align: justify; font-size: 18px;'>
+            Bienvenidos a la sección de la ruta de mi inolvidable viaje por Estados Unidos. 
+            Entre el 28 de julio de 2013 y el 15 de agosto de 2013, recorrimos Estados Unidos 
+            desde Nueva York hasta San Francisco en coche. Este viaje fue una experiencia única 
+            y llena de aventuras que ahora puedo compartir gracias a la tecnología.
+        </div>
+        <br>
+        <div style='text-align: justify; font-size: 18px;'>
+            Gracias a las fotos almacenadas en mi iPhone, he podido extraer las coordenadas de 
+            cada una y trazar la ruta exacta que seguimos. Cada marcador en el mapa representa 
+            una parada, una vista espectacular o un momento especial capturado durante este 
+            emocionante recorrido. Espero que disfruten este viaje virtual tanto como yo disfruté 
+            haciéndolo.
+        </div>
+        <br><br>
+    """, unsafe_allow_html=True)
+
     # Cargar el DataFrame con los metadatos
     df = pd.read_csv('data/metadata.csv')
 
@@ -25,5 +44,20 @@ def display():
                 icon=folium.Icon(color='blue', icon='info-sign')
             ).add_to(map)
 
-    # Mostrar el mapa en Streamlit
-    folium_static(map)
+    # Configurar columnas para centrar el mapa
+    col1, col2, col3 = st.columns([1, 6, 1])  # Ajustar el ancho de las columnas
+
+    with col2:
+        folium_static(map, width=1280, height=720)  # Ajusta el tamaño del mapa
+
+    st.markdown("""
+        <style>
+            .stApp {
+                max-width: 100%;
+                padding: 0;
+            }
+        </style>
+    """, unsafe_allow_html=True)
+
+# Llamar a la función display para mostrar la página
+display()
